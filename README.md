bac64031562e29b2ed730309569852f1f2a48aad
sharkpp <touroku@sharkpp.net>
2015-03-14 12:22:43 +0900

    Tumblr の投稿フォームのブログアイコンの位置を下部に変えるスクリプト追加

---
+++ README.md
@@ -0,0 +1,54 @@
+# Tumblr. blog icon to bottom
+
+このスクリプトは [Tumblr.](https://www.tumblr.com/)™ の投稿フォームのブログ選択アイコンの位置を上部から下部に移動させ上部までスクロールすること無くブログを切り替えることが出来るようにします。
+
+## インストール
+
+このスクリプトは [Greasy Fork](https://greasyfork.org/ja/scripts/xxxx) のページからインストールすることが出来ます。
+
+インストールおよび実行するには下記のような拡張が必要になります。
+
+* [Tampermonkey - Chrome ウェブストア](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=ja)
+* [Greasemonkey :: Add-ons for Firefox](https://addons.mozilla.org/ja/firefox/addon/greasemonkey/) ※動作未確認
+
+## アンインストール
+
+それぞれの拡張のヘルプを参照してください。
+
+## 機能の詳細
+
+このスクリプトを実行すると
+
+![適用前](img/before.png)
+
+と、投稿フォームの上部にあったブログアイコンとブログ名が
+
+![適用後](img/after.png)
+
+と、このようにブログの下部に移動します。
+
+アイコン類が下部にくることで投稿フォーム表示時に切り替えることが容易になります。
+
+## ライセンス
+
+The MIT License (MIT)
+
+Copyright (c) 2015 sharkpp.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ src/userscript.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name        Tumblr. blog icon to bottom
+// @namespace   http://www.sharkpp.net/
+// @version     0.1
+// @description Tumblr. blog icon to bottom in reblog form
+// @author      sharkpp
+// @copyright   2015, sharkpp
+// @license     MIT License
+// @include     https://www.tumblr.com/dashboard*
+// @include     https://www.tumblr.com/reblog/*
+// @include     https://www.tumblr.com/blog/*
+// @include     https://www.tumblr.com/tagged/*
+// @include     https://www.tumblr.com/search/*
+// ==/UserScript==
+(function ()
+{
+    var blogs = [];
+    var evaluate = function (xpath, resultOnce) {
+        resultOnce = undefined == typeof resultOnce ? false : resultOnce;
+        var items = document.evaluate(xpath, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+        return resultOnce ? (items.snapshotLength ? items.snapshotItem(0) : null)
+                          : items;
+    };
+    var fn = function () {
+            var postContainer = evaluate('//*[@class="post-container"]', true);
+            if (postContainer &&
+                !postContainer.getAttribute('data-sharkpp-icon-bottom'))
+            {
+                postContainer.setAttribute('data-sharkpp-icon-bottom', true);
+                //////////
+                var elm = evaluate('//*[@class="post-margin"]', true);
+                    elm.style.cssText = 'bottom:0; top:initial;';
+                var elm = evaluate('//*[contains(@class,\'post-content\')]', true);
+                    elm.style.cssText = 'top: 5px;';
+                var elm = evaluate('//*[contains(@class,\'post-header\')]', true);
+                    elm.style.cssText = 'position:absolute; bottom:5px; width:340px;';
+                var elm = evaluate('//*[@class="post-form--controls"]/*[@class="controls-container"]', true);
+                    elm.style.cssText = 'width:200px; margin-left:340px;';
+            }
+            setTimeout(fn, 1000);
+        };
+    setTimeout(fn, 1000);
+})();
