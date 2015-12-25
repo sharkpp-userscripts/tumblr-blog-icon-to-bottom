d3025c011297299157ba4aae5abc0e9e80004d3f
sharkpp <touroku@sharkpp.net>
2015-12-25 23:20:38 +0900

    デザインの変更で動作しなくなっていたのを修正

--- src/userscript.user.js
+++ src/userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Tumblr. blog icon to bottom
 // @namespace   http://www.sharkpp.net/
-// @version     0.3
+// @version     0.4
 // @description Tumblr. blog icon to bottom in reblog form
 // @author      sharkpp
 // @copyright   2015, sharkpp
@@ -28,14 +28,22 @@
             {
                 postContainer.setAttribute('data-sharkpp-icon-bottom', true);
                 //////////
-                var elm = evaluate('//*[@class="post-margin"]', true);
-                    elm.style.cssText = 'bottom:0; top:initial;';
-                var elm = evaluate('//*[contains(concat(" ",normalize-space(@class)," ")," post-content ")]', true);
+                var elm;
+                elm = evaluate('//*[@class="post-margin"]', true);
+                elm.style.cssText = 'bottom:0; top:initial;';
+                if (elm = evaluate('//*[contains(concat(" ",normalize-space(@class)," ")," post-content ")]', true)) { // old desigin
                     elm.style.cssText = 'top: 5px;';
-                var elm = evaluate('//*[contains(concat(" ",normalize-space(@class)," ")," post-header ")]', true);
+                    elm = evaluate('//*[contains(concat(" ",normalize-space(@class)," ")," post-header ")]', true);
                     elm.style.cssText = 'position:absolute; bottom:5px; width:340px;';
-                var elm = evaluate('//*[@class="post-form--controls"]/*[@class="controls-container"]', true);
+                    elm = evaluate('//*[@class="post-form--controls"]/*[@class="controls-container"]', true);
                     elm.style.cssText = 'width:200px; margin-left:340px;';
+                } else if (elm = evaluate('//*[contains(concat(" ",normalize-space(@class)," ")," post-form--form ")]', true)) {
+                    elm.style.cssText = 'top: 5px;';
+                    elm = evaluate('//*[contains(concat(" ",normalize-space(@class)," ")," post-form--header ")]', true);
+                    elm.style.cssText = 'position:absolute; bottom:5px; width:340px;';
+                    elm = evaluate('//*[@class="post-form--controls"]/*[@class="controls-container"]', true);
+                    elm.style.cssText = 'width:200px; margin-left:340px;';
+                }
             }
             setTimeout(fn, 1000);
         };
